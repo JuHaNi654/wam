e11239e3c97b2492543c4c0a1c64730454894e89
JuHaNi654 <JuHaNi654@users.noreply.github.com>
2026-05-25 08:25:21 +0300

    remove duplicate files

--- server-go/internal/models/actions.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Action struct {
-	ID    string `gorm:"primaryKey" json:"id"`
-	Title string `json:"title"`
-	Date  int64  `json:"date"`
-	Note  string `json:"note"`
-	JobID string `json:"job_id" gorm:"column:job_id"`
-}
